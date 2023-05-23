81efb96abbbb9d39d778a242cc2572fb28bbc9bf
sakibhosen98 <sakibhosen9815@gmail.com>
2023-05-23 19:46:23 +0600

    added car callection

--- src/Pages/Home/CarCollections/CarCollections.jsx
+++ src/Pages/Home/CarCollections/CarCollections.jsx
@@ -4,8 +4,8 @@ const CarCollections = () => {
       <h3 className="text-3xl font-bold text-center mb-8 gap-3">
         Top Car Collections
       </h3>
-      <div className="grid md:grid-cols-3 mx-auto">
-        <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="grid md:grid-cols-3 gap-3 mx-auto">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img
               src="https://i.ibb.co/cX6kPy4/fahad-bin-kamal-anik-QLaa3a-Ypwv8-unsplash.jpg"
@@ -20,7 +20,7 @@ const CarCollections = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img src="https://i.ibb.co/7SM2gdg/car-3.jpg" alt="Shoes" />
           </figure>
@@ -32,7 +32,7 @@ const CarCollections = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img
               src="https://i.ibb.co/cX6kPy4/fahad-bin-kamal-anik-QLaa3a-Ypwv8-unsplash.jpghttps://i.ibb.co/7GJFPqP/gimmel-magaway-4-E0u7-Vsf-Gms-unsplash.jpg"
@@ -47,7 +47,7 @@ const CarCollections = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img src="https://i.ibb.co/h86V3j0/car-5.jpg" alt="Shoes" />
           </figure>
@@ -59,7 +59,7 @@ const CarCollections = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img src="https://i.ibb.co/7SM2gdg/car-3.jpg" alt="Shoes" />
           </figure>
@@ -71,7 +71,7 @@ const CarCollections = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
           <figure>
             <img src="https://i.ibb.co/rGjC5W5/car-9.jpg" alt="Shoes" />
           </figure>
