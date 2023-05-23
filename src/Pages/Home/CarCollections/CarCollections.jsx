1231f01aeab9c8379c1a81a1f0a1dc0c0654cc1e
sakibhosen98 <sakibhosen9815@gmail.com>
2023-05-23 12:51:08 +0600

    added top car collection section

--- src/Pages/Home/CarCollections/CarCollections.jsx
+++ src/Pages/Home/CarCollections/CarCollections.jsx
@@ -1,25 +1,89 @@
-import { useEffect, useState } from "react";
-import CarDisplay from "./CarDisplay";
-
 const CarCollections = () => {
-  const [services, setservices] = useState([]);
-
-  useEffect(() => {
-    fetch('https://toy-car-server-gamma.vercel.app/carServices')
-    .then(res => res.json())
-    .then(data => setservices(data.slice(0,20)))
-  },[])
-
-  console.log(services)
-
   return (
-    <div>
-      {
-        services.map(service => <CarDisplay
-          key={service._id}
-          service={service}
-        ></CarDisplay>)
-      }
+    <div className="mt-8">
+      <h3 className="text-3xl font-bold text-center mb-8 gap-3">
+        Top Car Collections
+      </h3>
+      <div className="grid md:grid-cols-3 mx-auto">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/cX6kPy4/fahad-bin-kamal-anik-QLaa3a-Ypwv8-unsplash.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/7SM2gdg/car-3.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/cX6kPy4/fahad-bin-kamal-anik-QLaa3a-Ypwv8-unsplash.jpghttps://i.ibb.co/7GJFPqP/gimmel-magaway-4-E0u7-Vsf-Gms-unsplash.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/h86V3j0/car-5.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/7SM2gdg/car-3.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/rGjC5W5/car-9.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">T</h2>
+            <p>If a Car chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-secondary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import CarCollections from "../CarCollections/CarCollections";
 import FindCar from "../FindCar/FindCar";
 import Slideshow from "../Slideshow/Slideshow";
 import TabGroups from "../TabGroups/TabGroups";
@@ -9,6 +10,7 @@ const Home = () => {
         <Banner></Banner>
         <Slideshow></Slideshow>
         <TabGroups></TabGroups>
+        <CarCollections></CarCollections>
         <FindCar></FindCar>
     </div>
   );
